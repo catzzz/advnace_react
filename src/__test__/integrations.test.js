fff2e1831e90ca93c2ac1df30b02337662e0539c
Catzzz <chuneleu@gmail.com>
2021-11-17 11:32:33 -0800

    integration test with moxios

---
+++ src/__test__/integrations.test.js
@@ -0,0 +1,48 @@
+
+import React from 'react'
+import {mount} from 'enzyme'
+import Root from 'Root'
+import App from 'components/App'
+import moxios from 'moxios'
+
+beforeEach(() =>{
+    // intercept axios and use moxios instead
+    moxios.install()
+    moxios.stubRequest('http://jsonplaceholder.typicode.com/comments',{
+        status: 200,
+        response: [{name:'Fetch #1'},{name: 'Fetch #2'}]
+    })
+})
+
+afterEach(()=>{
+    moxios.uninstall();
+})
+it('can fetch a list of comments and display them', (done) =>{
+    // Attempt to render the entire app
+    const wrapped = mount(
+        <Root>
+            <App />
+        </Root>
+    )
+    // find the 'fetchComments' button and click it 
+    wrapped.find('.fetch-comments').simulate('click');
+    
+    
+    // introduce a tiny little pause
+
+    setTimeout(()=>{
+        // update ui
+        wrapped.update();
+         // Expect to find a list fo comments
+        expect(wrapped.find('li').length).toEqual(2);
+
+        // signal done to finish test;
+        done();
+        wrapped.unmount();
+    }, 100)
+
+   
+
+    
+
+ })
--- src/actions/__test__/index.test.js
+++ src/actions/__test__/index.test.js
@@ -13,7 +13,5 @@ describe('saveComment', () => {
     it('has the correct payload',() =>{
         const action = saveComment('New Comment');
         expect(action.payload).toEqual('New Comment');
-
-
     })
 })
